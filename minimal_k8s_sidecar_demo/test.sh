74aa869f214e24040c544977ca642808b6f2f9f2
Alex Diaz <alex@eadem.com>
2022-05-16 14:49:29 +0000

    -

--- minimal_k8s_sidecar_demo/test.sh
+++ minimal_k8s_sidecar_demo/test.sh
@@ -2,6 +2,6 @@
 echo -e '\033[48;5;166m => Testing wallarm sidecar with sqli and xss attacks \033[0m'
 
 #NODE_IP=$(kubectl get nodes --selector=node-role.kubernetes.io/master!= -o jsonpath={.items[0].status.addresses[0].address})
-NODE_IP=192.168.1.210
-(set -x; curl -k https://$NODE_IP/?id='or+1=1--a-<script>prompt(1)</script>' -H "host: localhost")
+NODE_IP=192.168.1.231
+(set -x; curl -k https://$NODE_IP/?id='or+1=1--a-<script>prompt(1)</script>' -H "host: sidecar.example.com")
 
